58ef721ab2da714fec655dd77b0b90d1311a3fc9
Jonathan Keane <jkeane889@gmail.com>
2019-02-21 07:31:33 -0500

    Created python program to determine Newtonian squareroot of number in python

---
+++ NewtonsMethod.py
@@ -0,0 +1,10 @@
+def newtonSqrt(n):
+    approx = 0.5 * n
+    better = 0.5 * (approx + n/approx)
+    while better != approx:
+        approx = better
+        better = 0.5 * (approx + n/approx)
+        print(better)
+    return approx
+
+print(newtonSqrt(25))
