0019eec001de74e7eb0eca8487e0f10c400eed3b
Jonathan Keane <jkeane889@gmail.com>
2019-02-21 14:40:59 -0500

    Created turtle graphic using for loop in Python

---
+++ ForLoopEx2.py
@@ -0,0 +1,11 @@
+import turtle
+wn = turtle.Screen()
+carlos = turtle.Turtle()
+
+for i in [1, 2]:
+    carlos.forward(175)
+    carlos.right(90)
+    carlos.forward(150)
+    carlos.right(90)
+
+wn.exitonclick()
