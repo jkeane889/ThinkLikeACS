afecbe9ef4e0a291bc73918d0c6dcc70157b5d6b
Jonathan Keane <jkeane889@gmail.com>
2019-02-21 07:27:55 -0500

    Used tkinter to create messagebox to open for user to continue

---
+++ messagebox.py
@@ -0,0 +1,8 @@
+# Yes/No Questions
+
+from tkinter import messagebox
+
+answer = messagebox.askokcancel("Question", "Do you want to open this file?")
+answer = messagebox.askretrycancel("Question", "Do you want to try that again?")
+answer = messagebox.askyesno("Question", "Do you like Python?")
+answer = messagebox.askyesnocancel("Question", "Continue playing?")
