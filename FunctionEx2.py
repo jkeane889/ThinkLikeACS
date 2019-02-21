9197412ab71f4baa9c4bf1bd9cafd8b49d8c9ba4
Jonathan Keane <jkeane889@gmail.com>
2019-02-21 14:42:33 -0500

    Created squareroots of numbers in an array

---
+++ FunctionEx2.py
@@ -0,0 +1,10 @@
+def square(x):
+    for counter in range(x):
+        runningtotal = 0
+        runningtotal = runningtotal + x
+
+    return runningtotal
+
+toSquare = 10
+squareResult = square(toSquare)
+print("The result of", toSquare, "squared is", squareResult)
